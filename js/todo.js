61e007a476175b84cd09047d61aaae967324eda8
onhope <chch4934@gmail.com>
2023-08-16 03:37:14 +0900

    DOM 다루기

---
+++ js/todo.js
@@ -0,0 +1,58 @@
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector(".todo-list");
+
+todoInput.addEventListener("keypress", (e)=> {
+  if (e.keyCode === 13) {
+    generateTodo(todoInput.value);
+    todoInput.value = "";
+  }
+})
+
+const generateTodo = (todo) => {
+  const li = document.createElement("li");
+  const likeSpan = generateLike();
+  const ItemSpan= generateItem(todo);
+  const ManageSpan = generateManage();
+
+  li.appendChild(likeSpan);
+  li.appendChild(ItemSpan);
+  li.appendChild(ManageSpan);
+
+  todoList.appendChild(li);
+}
+
+const generateLike = () => {
+  const span = document.createElement("span");
+  span.classList.add("todo-like");
+  const icon = document.createElement("i");
+  icon.classList.add("material-icons")
+  icon.classList.add("like")
+  icon.innerText = "favorite_border";
+  span.appendChild(icon);
+  return span;
+}
+
+const generateItem = (todo) => {
+  const span = document.createElement("span");
+  span.classList.add("todo-item");
+  span.innerText = todo;
+  return span;
+}
+
+const generateManage = () => {
+  const span = document.createElement("span");
+  span.classList.add("todo-manage");
+  const icon1 = document.createElement("i");
+  const icon2 = document.createElement("i");
+  icon1.classList.add("material-icons")
+  icon1.classList.add("check")
+  icon1.innerText = "check";
+  icon2.classList.add("material-icons")
+  icon2.classList.add("clear")
+  icon2.innerText = "clear";
+
+  span.appendChild(icon1);
+  span.appendChild(icon2);
+
+  return span;
+}
